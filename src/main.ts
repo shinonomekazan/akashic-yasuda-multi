a9bd56a089e221fa1a9deabcb509f6580d6de662
安田れもん <lemon.yasuda@gmail.com>
2021-01-07 15:47:31 +0900

    とりあえず発射できた

--- src/main.ts
+++ src/main.ts
@@ -1,141 +1,144 @@
-function main(param: g.GameMainParameterObject): void {
-	// ここにゲームコードを記述します
-	var scene = new g.Scene
-		({
-			game: g.game,
-			assetIds: ["aco", "aco2"],　
-		});
-	// シーンが読み込まれた時の処理をここに書く
-		scene.onLoad.addOnce(function ()
-	{
-		// acoちゃんを作る
-
-		var aco = new g.FrameSprite
-		({
-			scene: scene,
-			src: scene.asset.getImageById("aco"),
-			width: 32,
-			height: 48,
-			frames: [5, 6, 7, 6],
-			interval: 300
-		});
-		var aco2 = new g.FrameSprite({
-			scene: scene,
-			src: scene.asset.getImageById("aco2"),
-			width: 32,
-			height: 48,
-			x: 160,
-			y: 100,
-			frames: [5, 6, 4, 6],
-			interval: 300
-		});
-		var font = new g.DynamicFont({
-			game: g.game,
-			fontFamily: g.FontFamily.Serif,
-			size: 15
-		});
-		var label = new g.Label({
-			scene: scene,
-			font: font,
-			text: "gameover",
-			fontSize: 50,
-			textColor: "blue",
-			x: 100,
-			y: 100
-		});
-		var button = new g.FilledRect({
-			scene: scene,
-			cssColor: "yellow",
-			x: 10,
-			y: 400,
-			width: 80,
-			height: 50
-		});
-
-		var gameover = false
-
-		aco.x += 0;
-		aco.y += 100;
-		aco.modified();
-
-		//クリックした位置に移動
-		// scene.onPointDownCapture.add(function (ev) {
-		// 	if (gameover) {
-		// 		aco.x = 0;
-		// 		aco.y = 0;
-		// 		gameover = false
-
-		// 	}
-		// 	else
-		// 	{
-		// 		// タッチされたときの処理
-		// 		aco.y = ev.point.y;
-		// 	}
-		// });
-
-		aco.start();
-		aco2.start();
-		scene.append(aco);
-		scene.append(aco2);
-		
-		scene.append(button);
-		scene.append(label);
-		
-		scene.onPointDownCapture.add(function(ev) {
-			var size = 8;
-			var shot = new g.FilledRect({
-			  scene: scene,
-			  x: ev.point.x - size / 2,
-			  y: ev.point.y - size / 2,
-			  width: size,
-			  height: size - 6,
-			  cssColor: "#000000"
+	function main(param: g.GameMainParameterObject): void {
+		// ここにゲームコードを記述します
+		var scene = new g.Scene
+			({
+				game: g.game,
+				assetIds: ["aco", "aco2"],　
 			});
-			++shot.x;
-			shot.modified();
-			scene.append(shot);
-		});
+		// シーンが読み込まれた時の処理をここに書く
+			scene.onLoad.addOnce(function ()
+		{
+			// acoちゃんを作る
+
+			var aco = new g.FrameSprite
+			({
+				scene: scene,
+				src: scene.asset.getImageById("aco"),
+				width: 32,
+				height: 48,
+				frames: [5, 6, 7, 6],
+				interval: 300
+			});
+			var aco2 = new g.FrameSprite({
+				scene: scene,
+				src: scene.asset.getImageById("aco2"),
+				width: 32,
+				height: 48,
+				x: 160,
+				y: 100,
+				frames: [5, 6, 4, 6],
+				interval: 300
+			});
+			var font = new g.DynamicFont({
+				game: g.game,
+				fontFamily: g.FontFamily.Serif,
+				size: 15
+			});
+			var label = new g.Label({
+				scene: scene,
+				font: font,
+				text: "gameover",
+				fontSize: 50,
+				textColor: "blue",
+				x: 100,
+				y: 100
+			});
+			var button = new g.FilledRect({
+				scene: scene,
+				cssColor: "yellow",
+				x: 10,
+				y: 400,
+				width: 80,
+				height: 50
+			});
+
+			var gameover = false
+
+			aco.x += 0;
+			aco.y += 100;
+			aco.modified();
+
+			//クリックした位置に移動
+			// scene.onPointDownCapture.add(function (ev) {
+			// 	if (gameover) {
+			// 		aco.x = 0;
+			// 		aco.y = 0;
+			// 		gameover = false
+
+			// 	}
+			// 	else
+			// 	{
+			// 		// タッチされたときの処理
+			// 		aco.y = ev.point.y;
+			// 	}
+			// });
+
+			aco.start();
+			aco2.start();
+			scene.append(aco);
+			scene.append(aco2);
+			
+			scene.append(button);
+			scene.append(label);
+			
+			scene.onPointDownCapture.add(function(ev) {
+				var size = 8;
+				var shot = new g.FilledRect({
+				scene: scene,
+				x: ev.point.x - size / 2,
+				y: ev.point.y - size / 2,
+				width: size,
+				height: size - 6,
+				cssColor: "#000000"
+				});
+				// ++shot.x;
+				// shot.modified();
+				scene.append(shot);
+			
+
+			
+				scene.onUpdate.add(function () {
+
+					if (gameover)
+					{
+						aco.modified();
+					}
+					else
+					{
+						++aco.x;
+						aco.modified();
+						label.opacity = 0;
+					}
+
+					var yspritebox = aco.y + aco.height
+					var yrectbox = aco2.y + aco2.height
+					var xspritebox = aco.x + aco.width
+					var xrectbox = aco2.x + aco2.width
+
+					if (yspritebox >= aco2.y && yrectbox >= aco.y && xspritebox >= aco2.x && xrectbox >= aco.x)
+					{
+						console.log("当たり");
+						label.opacity = 1;
+						gameover = true
+					}
+					else
+					{
+						console.log("Win");
+					}
+
+					while (aco.x >= 811) {
+						console.log("出た");
+						aco.x = 0;
+
+					}
+					shot.x += 10;
+					shot.modified();
+				});
+			});
+		})
 
 		
-		scene.onUpdate.add(function () {
+		g.game.pushScene(scene);
+	}
 
-			if (gameover)
-			{
-				aco.modified();
-			}
-			else
-			{
-				++aco.x;
-				aco.modified();
-				label.opacity = 0;
-			}
-
-			var yspritebox = aco.y + aco.height
-			var yrectbox = aco2.y + aco2.height
-			var xspritebox = aco.x + aco.width
-			var xrectbox = aco2.x + aco2.width
-
-			if (yspritebox >= aco2.y && yrectbox >= aco.y && xspritebox >= aco2.x && xrectbox >= aco.x)
-			{
-				console.log("当たり");
-				label.opacity = 1;
-				gameover = true
-			}
-			else
-			{
-				console.log("Win");
-			}
-
-			while (aco.x >= 811) {
-				console.log("出た");
-				aco.x = 0;
-
-			}
-		});
-	})
-
-	
-	g.game.pushScene(scene);
-}
-
-export = main;
+	export = main;
